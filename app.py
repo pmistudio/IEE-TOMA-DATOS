5581a291e9ffe0b7f0eba918058ebfaf0d97fca6
pmistudio <haztelaidea@gmail.com>
2025-11-09 22:54:54 +0100

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-from datetime import datetime
-
-st.set_page_config(page_title="IEE · Toma de Datos", page_icon="📸", layout="centered")
-st.title("📸 IEE · Toma de datos en obra")
-
-st.write("Usa el menú lateral para capturar imágenes y rellenar la ficha del elemento.")
-st.sidebar.success("Selecciona una sección del menú.")
